d868c6477193851696a18e4fa4985f09eb83c741
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-05-01 10:15:29 -0400

    refactored app based on rubocop recs

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,134 +2,80 @@
 
 require 'bundler/setup'
 
+# Class `Bottles` generates verses of the 99 Bottles of Beer on the wall song
 class Bottles
-  attr_accessor :verse_int, :verse_array, :quantity, :containers_of_drink, :containers_location, :action_1, :action_2
+  attr_accessor :verse_int, :verse_array, :quantity, :containers_of_drink, :containers_location, :action1, :action2
 
   def initialize
     self.verse_array = []
   end
 
   def verse(int)
-    self.verse_array.clear
+    verse_array.clear
     self.verse_int = int
     set_lyrics
-    verse_array << "#{quantity.capitalize} #{containers_of_drink} #{containers_location},"
-    verse_array[0] += " #{quantity} #{containers_of_drink}."
-    verse_array << "#{action_1.capitalize} #{action_2},"
+    verse_array << "#{quantity.capitalize} #{containers_of_drink} #{containers_location}, #{quantity} #{containers_of_drink}."
+    verse_array << "#{action1.capitalize} #{action2},"
     decrement_verse_int
     set_lyrics
     verse_array[1] += " #{quantity} #{containers_of_drink} #{containers_location}."
-    verse_array.join("\n") + "\n"
+    "#{verse_array.join("\n")}\n"
   end
 
   def verses(arg1, arg2)
-    verses_array = create_inclusive_array_from_limits(arg1, arg2)
-    verses_array.map { |verse_number| verse(verse_number) }.join("\n")
+    array_of_verses = create_inclusive_array_from_limits(arg1, arg2)
+    array_of_verses.map { |verse_number| verse(verse_number) }.join("\n")
   end
 
   def song
-    verses(99,0)
+    verses(99, 0)
   end
 
   private
 
   def create_inclusive_array_from_limits(limit1, limit2)
-    ints = nil
-    if limit1 < limit2
-      ints = (limit1..limit2).to_a
-    elsif limit1 > limit2
-      ints = (limit2..limit1).to_a.reverse
-    else
-      raise ArgumentError
+    case limit1 <=> limit2
+    when -1 then (limit1..limit2).to_a
+    when 0  then raise ArgumentError
+    when 1  then (limit2..limit1).to_a.reverse
     end
   end
 
   def decrement_verse_int
-    self.verse_int -=1
-    self.verse_int = 99 if self.verse_int < 0
+    self.verse_int -= 1
+    self.verse_int = 99 if self.verse_int.negative?
   end
 
   def set_lyrics
     case verse_int
-    when 2..100
-      self.quantity = "#{verse_int}"
-      self.containers_of_drink = "bottles of beer"
-      self.containers_location = "on the wall"
-      self.action_1 = "take one down"
-      self.action_2 = "and pass it around"
-    when 1
-      self.quantity = "#{verse_int}"
-      self.containers_of_drink = "bottle of beer"
-      self.containers_location = "on the wall"
-      self.action_1 = "take it down"
-      self.action_2 = "and pass it around"
-    when 0
-      self.quantity = "no more"
-      self.containers_of_drink = "bottles of beer"
-      self.containers_location = "on the wall"
-      self.action_1 = "go to the store"
-      self.action_2 = "and buy some more"
+    when 2..100 then set_verses2upto100
+    when 1 then set_verse1
+    when 0 then set_verse0
     else raise ArgumentError
     end
   end
 
-  # def quantity
-  #   case verse_int
-  #   when 2..100
-  #   "#{verse_int}"
-  #   when 1
-  #   "#{verse_int}"
-  #   when 0
-  #   "no more"
-  #   else raise ArgumentError
-  #   end
-  # end
+  def set_verse0
+    self.quantity = 'no more'
+    self.containers_of_drink = 'bottles of beer'
+    self.containers_location = 'on the wall'
+    self.action1 = 'go to the store'
+    self.action2 = 'and buy some more'
+  end
 
-  # def containers_of_drink
-  #   case verse_int
-  #   when 2..100
-  #   "bottles of beer"
-  #   when 1
-  #   "bottle of beer"
-  #   when 0
-  #   "bottles of beer"
-  #   else raise ArgumentError
-  #   end
-  # end
+  def set_verse1
+    self.quantity = verse_int.to_s
+    self.containers_of_drink = 'bottle of beer'
+    self.containers_location = 'on the wall'
+    self.action1 = 'take it down'
+    self.action2 = 'and pass it around'
+  end
 
-  # def containers_location
-  #   "on the wall"
-  # end
-
-  # def action_1
-  #   case verse_int
-  #   when 2..100
-  #   "take one down"
-  #   when 1
-  #   "take it down"
-  #   when 0
-  #   "go to the store"
-  #   else raise ArgumentError
-  #   end
-  # end
-
-#   def action_2
-#     case verse_int
-#     when 2..100
-#     "and pass it around"
-#     when 1
-#     "and pass it around"
-#     when 0
-#     "and buy some more"
-#     else raise ArgumentError
-#     end
-#   end
+  def set_verses2upto100
+    self.quantity = verse_int.to_s
+    self.containers_of_drink = 'bottles of beer'
+    self.containers_location = 'on the wall'
+    self.action1 = 'take one down'
+    self.action2 = 'and pass it around'
+  end
 end
-
-# bottle_instance = Bottles.new
-# z = bottle_instance.verse(1)
-# p z
-# p z
-# y = bottle_instance.verses(99, 97)
-# p y
-# bottle_instance.song
